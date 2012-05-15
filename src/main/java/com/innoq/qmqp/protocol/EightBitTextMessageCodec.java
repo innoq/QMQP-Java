cb67e7fa32941ea27b3a21df302ffb675e0ae91f
Stefan Bodewig <stefan.bodewig@innoq.com>
2012-05-15 17:44:37 +0200

    Newline-Handling

---
+++ src/main/java/com/innoq/qmqp/protocol/EightBitTextMessageCodec.java
@@ -0,0 +1,78 @@
+/*
+  Copyright (C) 2012 innoQ Deutschland GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.innoq.qmqp.protocol;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Simple Encoder for 8 bit text messages as defined by http://cr.yp.to/proto/8bittext.html
+ *
+ * <p>Convenience methods and support for decoding may be added later.</p>
+ */
+public class EightBitTextMessageCodec {
+
+    private static final byte LF = 012;
+    private static final byte CR = 015;
+
+    /**
+     * Transforms an array of bytes into an 8 bit text message in
+     * which lines are separated by \012.
+     */
+    public byte[] toMessage(byte[] b) {
+        if (b != null) {
+            boolean lastByteWasCr = false;
+            final int len = b.length;
+            for (int i = 0; i < len; i++) {
+                if (b[i] == CR) {
+                    lastByteWasCr = true;
+                    continue;
+                }
+                if (lastByteWasCr && b[i] == LF) {
+                    return encodeToMessage(b, i - 1);
+                }
+                lastByteWasCr = false;
+            }
+        }
+        return b;
+    }
+
+    private byte[] encodeToMessage(byte[] b, int knownGoodChars) {
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        if (knownGoodChars > 0) {
+            bos.write(b, 0, knownGoodChars);
+        }
+        bos.write(LF);
+
+        boolean lastByteWasCr = false;
+        final int len = b.length;
+        for (int i = knownGoodChars + 2; i < len; i++) {
+            if (b[i] == CR) {
+                lastByteWasCr = true;
+                continue;
+            }
+            if (lastByteWasCr && b[i] != LF) {
+                bos.write(CR);
+            }
+            bos.write(b[i]);
+            lastByteWasCr = false;
+        }
+        if (lastByteWasCr) {
+            bos.write(CR);
+        }
+        return bos.toByteArray();
+    }
+}
---
+++ src/test/java/com/innoq/qmqp/protocol/EightBitTextMessageCodecTest.java
@@ -0,0 +1,85 @@
+/*
+  Copyright (C) 2012 innoQ Deutschland GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.innoq.qmqp.protocol;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EightBitTextMessageCodecTest {
+
+    @Test
+    public void nullIsEncodedAsNull() {
+        Assert.assertNull(new EightBitTextMessageCodec().toMessage(null));
+    }
+
+    @Test
+    public void messageOfSingleLineIsReturnedAsIs() {
+        final byte[] b = new byte[] {
+            'a', ' ', 's', 'i', 'n', 'g', 'l', 'e', ' ', 'l', 'i', 'n', 'e'
+        };
+        Assert.assertSame(b, new EightBitTextMessageCodec().toMessage(b));
+    }
+
+    @Test
+    public void messageWithUnixEolsIsReturnedAsIs() {
+        final byte[] b = new byte[] {
+            'o', 'n', ' ', 'm', 'u', 'l', 't', 'i', 'p', 'l', 'e', '\n',
+            'l', 'i', 'n', 'e', 's'
+        };
+        Assert.assertSame(b, new EightBitTextMessageCodec().toMessage(b));
+    }
+
+    @Test
+    public void messageWithCrsOnlyIsReturnedAsIs() {
+        final byte[] b = new byte[] {
+            'o', 'n', ' ', 'm', 'u', 'l', 't', 'i', 'p', 'l', 'e', '\r',
+            'l', 'i', 'n', 'e', 's'
+        };
+        Assert.assertSame(b, new EightBitTextMessageCodec().toMessage(b));
+    }
+
+    @Test
+    public void messageWithWindowsEolsIsModified() {
+        final byte[] b = new byte[] {
+            'o', 'n', ' ', 'm', 'u', 'l', 't', 'i', 'p', 'l', 'e', '\r', '\n',
+            'l', 'i', 'n', 'e', 's'
+        };
+        final byte[] expected = new byte[] {
+            'o', 'n', ' ', 'm', 'u', 'l', 't', 'i', 'p', 'l', 'e', '\n',
+            'l', 'i', 'n', 'e', 's'
+        };
+        Assert.assertArrayEquals(expected,
+                                 new EightBitTextMessageCodec().toMessage(b));
+    }
+
+    @Test
+    public void emptyLinesJustChangeEol() {
+        final byte[] b = new byte[] { '\r', '\n', '\r', '\n' };
+        final byte[] expected = new byte[] { '\n', '\n' };
+        Assert.assertArrayEquals(expected,
+                                 new EightBitTextMessageCodec().toMessage(b));
+    }
+
+    @Test
+    public void trailingCrsArePreserved() {
+        final byte[] b = new byte[] { '\r', '\n', '\r', };
+        final byte[] expected = new byte[] { '\n', '\r' };
+        Assert.assertArrayEquals(expected,
+                                 new EightBitTextMessageCodec().toMessage(b));
+    }
+
+}
