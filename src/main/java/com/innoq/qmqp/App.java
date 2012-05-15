dd859f41f69e5b4164d475f2be559ca3cf39aa08
Stefan Bodewig <stefan.bodewig@innoq.com>
2012-05-15 11:08:43 +0200

    skeleton project structure

---
+++ src/main/java/com/innoq/qmqp/App.java
@@ -0,0 +1,13 @@
+package com.innoq.qmqp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
